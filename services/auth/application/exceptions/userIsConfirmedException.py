ee72f6f4b11e54adc4f1e78da0f6d786ed518471
Marcin Pyrka <pyrka.marcin@gmail.com>
2020-07-14 09:28:29 +0200

    Remove auth poc

--- services/auth/application/exceptions/userIsConfirmedException.py
+++
@@ -1,8 +0,0 @@
-from application.models.users import Users
-
-
-class UserIsConfirmedException(Exception):
-    def __init__(self, message, user: Users):
-        super(Exception, self).__init__()
-        self.message = message
-        self.user = user
--- services/auth/run.py
+++
@@ -1,4 +0,0 @@
-import application
-
-if __name__ == '__main__':
-    application.create_app().run(debug=True, host='0.0.0.0', use_reloader=True)
