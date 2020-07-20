d94a4cd8e533f0c8aecdb893a0ce77685c44e2ed
Marcin Pyrka <pyrka.marcin@gmail.com>
2020-07-20 10:19:24 +0200

    Upgrade

---
+++ services/backend/src/views/Confirmation.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="Confirmation">
+    <pre v-text="$attrs"/>
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    name: 'Confirmation',
+    data() {
+      return {};
+    },
+    computed: {},
+    methods: {},
+    mounted() {
+
+      this.$store.dispatch('auth/confirmation', this.$route.query.token).then(
+        data => {
+          this.message = data.message;
+          this.successful = true;
+
+          this.$router.push('/login');
+        },
+        error => {
+          this.message =
+            (error.response && error.response.data) ||
+            error.message ||
+            error.toString();
+          this.successful = false;
+        }
+      );
+    },
+    props: {}
+  }
+</script>
---
+++ services/backend/src/views/Profile.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="container">
+    <header class="jumbotron">
+      <h3>
+        <strong>{{currentUser.username}}</strong> Profile
+      </h3>
+    </header>
+    <p>
+      <strong>Token:</strong>
+      {{currentUser.access_token}}
+    </p>
+    <p>
+      <strong>Ref Token:</strong>
+      {{currentUser.refresh_token}}
+    </p>
+    <p>
+      <strong>Id:</strong>
+      {{currentUser.id}}
+    </p>
+    <strong>Authorities:</strong>
+    <ul>
+      <li v-for="(role,index) in currentUser.roles" :key="index">{{role}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Profile',
+    computed: {
+      currentUser() {
+        return this.$store.state.auth.user;
+      }
+    },
+    mounted() {
+      if (!this.currentUser) {
+        this.$router.push('/login');
+      }
+    }
+  };
+</script>
